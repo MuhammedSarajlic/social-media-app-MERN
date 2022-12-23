ae9b34e6d71bbdd6b2c6c7fbc8b7495bcba5e3eb
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2022-12-24 00:27:12 +0100

    Feat: Add Tailwind login and register components

--- client/src/container/Login.jsx
+++ client/src/container/Login.jsx
@@ -44,29 +44,100 @@ const Login = ({ setIsAuthenticated }) => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label for="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
-        <label for="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        {error && <p>{error}</p>}
-        <button>Login</button>
-        <p>
-          Don't have an account <Link to="/register">Register</Link>
-        </p>
-      </form>
+      <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="w-full max-w-md space-y-8">
+          <div>
+            <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+
+          {/* Form start here */}
+
+          <form class="mt-8 space-y-6" onSubmit={handleSubmit} method="POST">
+            <input type="hidden" name="remember" value="true" />
+            <div class="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label for="email-address" class="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label for="password" class="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autocomplete="current-password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            {error && <p className="text-red-600 ">{error}</p>}
+
+            <div class="flex items-center justify-between">
+              <div class="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
+                <label
+                  for="remember-me"
+                  class="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div class="text-sm">
+                <a
+                  href="#"
+                  class="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Sign in
+              </button>
+            </div>
+            <p>
+              Don't have an account?{" "}
+              <Link
+                to="/register"
+                className="text-indigo-600 hover:text-indigo-500"
+              >
+                Register
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
--- client/src/container/Register.jsx
+++ client/src/container/Register.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
 const Register = () => {
@@ -59,51 +59,102 @@ const Register = () => {
 
   return (
     <>
-      Register
-      <form onSubmit={handleRegister}>
-        <label for="firstName">First name</label>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          value={user.firstName}
-          onChange={handleChange}
-          placeholder="First Name"
-          required
-        />
-        <label for="lastName">Last name</label>
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          value={user.lastName}
-          onChange={handleChange}
-          placeholder="Last Name"
-          required
-        />
-        <label for="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={user.email}
-          onChange={handleChange}
-          placeholder="Email"
-          required
-        />
-        <label for="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={user.password}
-          onChange={handleChange}
-          placeholder="Password"
-          required
-        />
-        {error && <p>{error}</p>}
-        <button>Register</button>
-      </form>
+      <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="w-full max-w-md space-y-8">
+          <div>
+            <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Create account
+            </h2>
+          </div>
+
+          {/* Form start here */}
+
+          <form class="mt-8 space-y-6" onSubmit={handleRegister} method="POST">
+            <input type="hidden" name="remember" value="true" />
+            <div class="-space-y-px rounded-md shadow-sm">
+              <div className="flex">
+                <label for="firstName" class="sr-only">
+                  First Name
+                </label>
+                <input
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  required
+                  value={user.firstName}
+                  onChange={handleChange}
+                  class="relative block w-full appearance-none rounded-none rounded-tl-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="First Name"
+                />
+                <label for="lastName" class="sr-only">
+                  Last Name
+                </label>
+                <input
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  required
+                  value={user.lastName}
+                  onChange={handleChange}
+                  class="relative block w-full appearance-none rounded-none rounded-tr-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Last Name"
+                />
+              </div>
+              <div>
+                <label for="email-address" class="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  required
+                  value={user.email}
+                  onChange={handleChange}
+                  class="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label for="password" class="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autocomplete="current-password"
+                  required
+                  value={user.password}
+                  onChange={handleChange}
+                  class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            {error && <p className="text-red-600 ">{error}</p>}
+
+            <div>
+              <button
+                type="submit"
+                class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Register
+              </button>
+            </div>
+            <p>
+              Already have an account?{" "}
+              <Link
+                to="/login"
+                className="text-indigo-600 hover:text-indigo-500"
+              >
+                Sign In
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
