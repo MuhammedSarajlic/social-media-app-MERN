a32da3e228743daabc1c5e7002034c41d6a8e560
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2022-12-26 00:25:55 +0100

    Feat: Home header style

--- client/src/container/Home.jsx
+++ client/src/container/Home.jsx
@@ -1,9 +1,11 @@
-import axios from "axios";
 import Cookies from "js-cookie";
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { BellIcon } from "@heroicons/react/24/outline";
 
-const Home = ({ setIsAuthenticated, user, setUser }) => {
+const Home = ({ setIsAuthenticated, user }) => {
   const navigate = useNavigate();
 
   const handleLogOut = () => {
@@ -16,10 +18,81 @@ const Home = ({ setIsAuthenticated, user, setUser }) => {
 
   return (
     <>
-      {console.log(user)}
-      Home
       <p>{user?.user.firstName}</p>
-      <button onClick={handleLogOut}>Log out</button>
+      {/*Here it starts */}
+      <Disclosure as="nav" className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="flex flex-1 items-center justify-start sm:items-stretch sm:justify-start">
+              <div className="flex flex-shrink-0 items-center">
+                <img
+                  className="block h-8 w-auto lg:hidden"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Your Company"
+                />
+                <img
+                  className="hidden h-8 w-auto lg:block"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Your Company"
+                />
+              </div>
+            </div>
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <button
+                type="button"
+                className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              >
+                <span className="sr-only">View notifications</span>
+                <BellIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+
+              {/* Profile dropdown */}
+              <Menu as="div" className="relative ml-3">
+                <div>
+                  <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src={user.user.imageUrl}
+                      alt=""
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Item>
+                      <a className={"block px-4 py-2 text-sm text-gray-700"}>
+                        Your Profile
+                      </a>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <a className={"block px-4 py-2 text-sm text-gray-700"}>
+                        Settings
+                      </a>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <button
+                        onClick={handleLogOut}
+                        className={"block px-4 py-2 text-sm text-gray-700"}
+                      >
+                        Sign out
+                      </button>
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            </div>
+          </div>
+        </div>
+      </Disclosure>
     </>
   );
 };
